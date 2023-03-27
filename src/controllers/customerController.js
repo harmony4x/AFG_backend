63bd3d86c40fdcfbd9d70fbc353e9c4386729e57
harmony4x <109467130+harmony4x@users.noreply.github.com>
2023-03-27 15:19:32 +0700

    update code Create array Customer

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -4,35 +4,54 @@ const { uploadSingleFile } = require('../services/fileService');
 
 module.exports = {
     createCustomerUser: async (req, res) => {
-        let { name, email, adress, phone, description } = req.body;
-        let imageUrl = '';
-        if (!req.files || Object.keys(req.files).length === 0) {
+        if (req.body.customers = true && Array.isArray(req.body.customers)) {
+            let data = req.body.customers;
 
+            let result = await createArrayCustomerService(data);
+            if (result) {
+                return res.status(200).json({
+                    errorCode: 0,
+                    data: result
+                })
+            } else {
+                return res.status(200).json({
+                    errorCode: -1,
+                    data: null
+                })
+            }
         } else {
-            file = req.files.image;
-            let result = await uploadSingleFile(file);
-            imageUrl = result.path;
-        }
-        let customerData = {
-            name, email, adress, phone, description, imageUrl
-        }
 
-        let customer = await createCustomerService(customerData);
-        if (customer) {
-            return res.status(200).json({
-                errorCode: 0,
-                data: customer
-            })
-        } else {
-            return res.status(200).json({
-                errorCode: -1,
-                data: ""
-            })
-        }
 
+            let { name, email, adress, phone, description } = req.body;
+            let imageUrl = '';
+            if (!req.files || Object.keys(req.files).length === 0) {
+
+            } else {
+                file = req.files.image;
+                let result = await uploadSingleFile(file);
+                imageUrl = result.path;
+            }
+            let customerData = {
+                name, email, adress, phone, description, imageUrl
+            }
+
+            let customer = await createCustomerService(customerData);
+            if (customer) {
+                return res.status(200).json({
+                    errorCode: 0,
+                    data: customer
+                })
+            } else {
+                return res.status(200).json({
+                    errorCode: -1,
+                    data: ""
+                })
+            }
+        }
     },
     createArrayUser: async (req, res) => {
         let data = req.body.customers;
+
         let result = await createArrayCustomerService(data);
         if (result) {
             return res.status(200).json({
